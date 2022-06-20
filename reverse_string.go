f0f6597be31a7fcce80e4ef3c66698d6b75b518c
smalay <smalay@slb.com>
2022-06-20 11:38:01 +0530

    decrementing the index

--- reverse_string.go
+++ reverse_string.go
@@ -2,7 +2,7 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	for i := len(input) - 1; i >= 0; i++ {
+	for i := len(input) - 1; i >= 0; i-- {
 		output += string(input[i])
 	}
 	return output
